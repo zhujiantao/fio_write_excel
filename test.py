37d4dbbfd7fd0694871d5ae466144996d91f21f4
zhujiantao <zhujt@winhong.com>
2018-01-08 15:54:43 +0800

    保持excel页和页内内容顺序固定
    保持excel页面顺序与字典中定义的顺序一致
    将excel每页内容固定顺序为"bw", "iops", "min", "max", "avg"

--- test.py
+++ test.py
@@ -9,14 +9,14 @@ workbook = getWorkbook()
 row = 1
 
 for io_mode_key in io_mode.keys():
-    io_mode_value = io_mode.get(io_mode_key)
+    io_mode_value = io_mode[io_mode_key]
     writeHeaderToExcel(workbook, io_mode_value)
 
 for file_name in fiofile_tuple:
     for grep_content in grep_content_tuple:
-        bw_iops_lat_dict = get_bw_iops_lat_dict(file_name, grep_content,  bw_iops_line_num, clat_line_num)
-        io_mode_key = grep_content.split(":")[0]
-        writeToExcel(workbook, io_mode.get(io_mode_key), row, 2, bw_iops_lat_dict)
+        bw_iops_lat_ordereddict = get_bw_iops_lat_dict(file_name, grep_content, bw_iops_line_num, clat_line_num)
+        io_mode_key = grep_content.split(":")[0][1:]
+        writeToExcel(workbook, io_mode.get(io_mode_key), row, 2, bw_iops_lat_ordereddict)
     row = row + 1
 
 for io_mode_key in io_mode.keys():
